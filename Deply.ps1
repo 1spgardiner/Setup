53657a6eb19fed770d7152cbcbfcdc91acf7013f
Stephen <sp_gardiner@hotmail.com>
2021-11-13 19:09:33 +0000

    Base files

---
+++ Deply.ps1
@@ -0,0 +1,57 @@
+choco install   Chocolately -y 
+choco install   git -y 
+git config — global user.email “sp_gardiner@hitmail.com”
+choco install   powershell-core -y 
+choco install   azure-cli -y 
+choco install   azurepowershell -y 
+choco install   vscode -y 
+choco install   vcredist-all -y 
+choco install   docker-desktop -y 
+choco install   vscode-docker -y 
+choco install 7-zip
+choco install Adobe-reader
+choco install itunes
+choco install greenshot
+choco install keepass
+choco install irfanviewer
+choco install mRemoteNG
+choco install Notepad++
+choco install vcredist-all
+choco install winscp
+choco install zoom
+choco install terminal
+
+
+###endregion
+#Install Visual Studio Extensions
+code --install-extension 4ops.terraform
+code --install-extension eamodio.gitlens
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension hashicorp.terraform
+code --install-extension laszer25.terraform-azure-autocomplete
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-azuretools.vscode-azureresourcegroups
+code --install-extension ms-azuretools.vscode-azurestorage
+code --install-extension ms-azuretools.vscode-azureterraform
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.azurecli
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ms-vscode.notepadplusplus-keybindings
+code --install-extension ms-vscode.powershell
+code --install-extension msazurermtools.azurerm-vscode-tools
+code --install-extension rosshamish.kuskus-kusto-language-server
+code --install-extension rosshamish.kuskus-kusto-syntax-highlighting
+code --install-extension Tyriar.shell-launcher
+code --install-extension VisualStudioExptTeam.vscodeintellicode
